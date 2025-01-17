ab3c2eacae72e908d624409abd696ff8cff12b2f
hariharan2701-IT <hariharan27012004@gmail.com>
2025-01-17 19:58:35 +0530

    40% frontend over

--- snapzone-web/src/App.jsx
+++ snapzone-web/src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Popup from "./components/Popup";
 import Frame from "./components/Frame";
 import framesData from "./frames.json";
 
 const App = () => (
   <div>
-    <Popup />
+    
     <Header />
     <div className="sale-container">
       <div className="sale-text">THANKS FOR CHOOSING SNAPZONE FRAMES! ❤️</div>
