8ebbd26e99791660403212c9517c93b67e1c3d6f
hariharan2701-IT <hariharan27012004@gmail.com>
2025-02-20 01:52:03 +0530

    added features

--- snapzone-web/src/App.jsx
+++ snapzone-web/src/App.jsx
@@ -5,6 +5,8 @@ import Frame from "./components/Frame";
 import Customer from "./components/Customer";
 import Categories from "./components/Categories";
 import framesData from "./frames.json";
+import MirrorFrames from "./mirrorframes.json";
+import flowers from "./flower.json";
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState("Frames");
@@ -51,6 +53,49 @@ const App = () => {
                 )
               ))}
             </section>
+            
+          )}
+
+
+{selectedCategory === "Magic Mirror Frame" && (
+            <section className="frame-section p-4">
+              <h2 className="text-2xl font-semibold mb-4">Magic Mirror Frames</h2>
+
+              {["Various"].map((size) => (
+                MirrorFrames[size]?.length > 0 && (
+                  <div key={size} className="frames-section mt-8">
+                    <h3 className="text-xl font-semibold mb-4 capitalize">{size} Collection</h3>
+                    <div className="frames grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                      {MirrorFrames[size].map((frame, index) => (
+                        <Frame key={index} frame={frame} onClick={() => handleFrameClick(frame)} />
+                      ))}
+                    </div>
+                  </div>
+                )
+              ))}
+            </section>
+            
+          )}
+
+          
+{selectedCategory === "Flower Bouquet" && (
+            <section className="frame-section p-4">
+              <h2 className="text-2xl font-semibold mb-4">All Frame Sizes</h2>
+
+              {["small", "medium", "large"].map((size) => (
+                flowers[size]?.length > 0 && (
+                  <div key={size} className="frames-section mt-8">
+                    <h3 className="text-xl font-semibold mb-4 capitalize">{size} Frames</h3>
+                    <div className="frames grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                      {flowers[size].map((frame, index) => (
+                        <Frame key={index} frame={frame} onClick={() => handleFrameClick(frame)} />
+                      ))}
+                    </div>
+                  </div>
+                )
+              ))}
+            </section>
+            
           )}
 
           {/* Selected Frame Details */}
